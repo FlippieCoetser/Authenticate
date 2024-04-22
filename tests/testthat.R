678f7c60378b56a7eda305c31ace227d34e4a1d6
Flippie Coetser <flippie.coetser@gmail.com>
2024-04-22 04:20:41 -0700

    PACKAGE: Update Metadata

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(R.Template)
+library(Authenticate)
 
-test_check("R.Template")
+test_check("Authenticate")
