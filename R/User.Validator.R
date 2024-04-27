5780d4f4e090949a19a885b8bb24c84fa26635a2
Flippie Coetser <flippie.coetser@gmail.com>
2024-04-27 09:12:18 -0700

    DOCUMENTATION: Update User.Validator Documents

--- R/User.Validator.R
+++ R/User.Validator.R
@@ -3,21 +3,16 @@
 #' This function creates a suite of validators for checking the integrity of user data.
 #' It encapsulates various checks to ensure that user objects meet expected criteria,
 #' such as non-null values for essential attributes and specific format validations.
-#' Each validator function is designed to throw a specific exception from 
-#' `User.Validation.Exceptions` if the validation fails.
+#' Each validator function is designed to throw a specific exception from
+#' \code{User.Validation.Exceptions} if the validation fails.
 #'
-#' The validators include checks for:
-#'   - Existence of the user object.
-#'   - Presence of essential attributes like ID, username, hash, and salt.
-#'   - Specific format checks, such as UUID format for ID.
-#'
-#' @return A list of validator functions which include:
-#'   \itemize{
-#'     \item{\code{User}:}{Validates a complete user object by sequentially applying all individual attribute checks.}
-#'     \item{\code{Exists}:}{Checks if the user object is not NULL.}
-#'     \item{\code{HasId}, \code{HasUsername}, \code{HasHash}, \code{HasSalt}:}{Ensure that each respective attribute is not NULL.}
-#'     \item{\code{Id}:}{Validates that the user ID is in UUID format.}
-#'   }
+#' @return A list of validator functions, which includes:
+#' \describe{
+#'   \item{\code{User}}{Validates a complete user object by sequentially applying all individual attribute checks.}
+#'   \item{\code{Exists}}{Checks if the user object is not NULL.}
+#'   \item{\code{HasId}, \code{HasUsername}, \code{HasHash}, \code{HasSalt}}{Ensures that each respective attribute is not NULL.}
+#'   \item{\code{Id}}{Validates that the user ID is in UUID format.}
+#' }
 #' @export
 User.Validator <- \() {
   exception <- User.Validation.Exceptions()
